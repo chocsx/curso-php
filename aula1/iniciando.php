f036056d7005282e5c6bdcc06bb1ae2fb7af4020
Mateus Araujo <chocsx@hotmail.com>
2016-08-31 17:09:41 -0300

    iniciando php

---
+++ aula1/iniciando.php
@@ -0,0 +1,15 @@
+<?php
+  $var = "Mateus";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Curso PHP</title>
+  </head>
+  <body>
+      <p>
+        Olá, <?= $var ?>
+      </p>
+  </body>
+</html>
