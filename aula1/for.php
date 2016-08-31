0615b53bc3be6aa2bc7c68747458d88b9119b9bf
Mateus Araujo <chocsx@hotmail.com>
2016-08-31 17:27:51 -0300

    IF e FOR no PHP

---
+++ aula1/for.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 17;
+
+if ($idade > 18):
+  echo "resultado";
+elseif($idade == 17):
+  echo "resultado CERTO";
+else:
+  echo "resultado 2";
+endif;
---
+++ aula1/if.php
@@ -0,0 +1,12 @@
+<?php
+
+$numeros = 10;
+
+//INCREMENTA
+for ($i=1; $i < $numeros; $i++) {
+  echo $i;
+}
+//DECREMENTA
+for ($i=0; $i <= $numeros; $numeros--) {
+  echo $numeros."<br>";
+}
