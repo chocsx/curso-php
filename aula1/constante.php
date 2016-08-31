697ac8b38b9040968a9449c406d17434ef372e1a
Mateus Araujo <chocsx@hotmail.com>
2016-08-31 17:21:20 -0300

    constantes no php

---
+++ aula1/constante.php
@@ -0,0 +1,20 @@
+<?php
+
+define('CSS','css/');
+define('JS','js/');
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Curso php</title>
+    <script src="<?php echo CSS."style.css" ?>" charset="utf-8"></script>
+  </head>
+  <body>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    <script src="<?= JS."js.js"  ?>" type="text/javascript">    </script>
+  </body>
+</html>
