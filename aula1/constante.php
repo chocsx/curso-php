ed586060f169d399ce713d4c7d54221b936c7c77
Wesllen Masolliny <wesllenalves@gmail.com>
2016-09-09 17:05:12 -0300

    constantes no php

---
+++ aula1/constante.php
@@ -0,0 +1,20 @@
+<?php
+define('CSS', 'css/');
+define('JS','js/');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf8">
+<title>Curso de php</title>
+<script src="<?=CSS."style.css" ?>"></script>
+</head>
+<body>
+<p>
+Para entrar no grupo oficial do Curso de PHP basta seguir o link https://www.facebook.com/events/1045083332276749/,
+lembrando que só é permitido postagens e discussões sobre dúvidas e/ou assuntos pertinentes ao curso, aquele que
+não respeitar essas regras será banido.
+</p>
+<script src="<?= JS.'js.js'?>" type="text/javascript"></script>
+</body>
+</html>
