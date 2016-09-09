069a26f16d7aa9080a931ae831328b0468bb534b
Wesllen Masolliny <wesllenalves@gmail.com>
2016-09-09 17:35:28 -0300

    If e For no PHP

---
+++ aula1/if.php
@@ -0,0 +1,19 @@
+<?php
+/*$idade = 22;
+if ($idade < 18):
+  echo "Voce é Maior de idade!";
+elseif ($idade = 22):
+  echo "Voce é maior duas vezes!";
+else:
+  echo "Voce não é maior de idade!";
+endif;*/
+
+$numeros = 10;
+//INCREMENTA
+for ($i=1; $i <= $numeros; $i++) {
+  echo $i;
+}
+//DECREMENTA
+  for ($i=1; $i <= $numeros; $numeros--) {
+    echo $numeros."<br>";
+}
